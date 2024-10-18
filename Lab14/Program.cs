11938271b5f3407cafc8c8065a2d90328ee92fb5
Илья <@DESKTOP-VPI29MR>
2024-10-18 09:55:28 +0200

    Добавьте файлы проекта.

---
+++ Lab14/Program.cs
@@ -0,0 +1,45 @@
+﻿try
+{
+
+    Console.Write("Введите номер месяца (от 1 до 12): ");
+
+    
+    int month;
+
+    
+    bool isValidInput = int.TryParse(Console.ReadLine(), out month);
+
+    if (!isValidInput || month < 1 || month > 12)
+    {
+        Console.WriteLine("Некорректный номер месяца. Пожалуйста, введите число от 1 до 12.");
+        return;
+    }
+
+    
+    string season;
+
+    if (month >= 3 && month <= 5)
+    {
+        season = "весна";
+    }
+    else if (month >= 6 && month <= 8)
+    {
+        season = "лето";
+    }
+    else if (month >= 9 && month <= 11)
+    {
+        season = "осень";
+    }
+    else // месяц 12, 1, 2
+    {
+        season = "зима";
+    }
+
+    
+    Console.WriteLine($"Сезон: {season}");
+
+}
+catch 
+{
+    Console.WriteLine("Error");
+}
